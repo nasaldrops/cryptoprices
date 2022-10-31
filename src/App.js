30a2444e0e2318c9a25c3fd218f3348f9f3ff1a4
Kevin O'Connor <hellokevinoconnor@gmail.com>
2022-10-31 21:59:23 +0000

    added input field

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios';
-
-
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-
-  const [price, setPrice] = useState('');
-
-
+  const [price, setPrice] = useState("");
+  const [cPair, setPair] = useState("");
 
   const fetchPrice = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=SHIBBUSD');
-    console.log(result.data.price)
-    setPrice(result.data.price)
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=" + cPair
+    );
+    console.log(result.data.price);
+    setPrice(result.data.price);
+  };
 
+  const cryptoPair = (event) => {
+    console.log(event.target.value);
+    setPair(event.target.value);
+  };
 
-
-
+  const getPrice = async () => {
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=SHIBBUSD"
+    );
+    console.log(result.data.price);
+    setPrice(result.data.price);
   };
 
   return (
     <div className="container">
       <div>
-        <h1 className="title"> { price } </h1>
-        <button onClick={fetchPrice} className="btn btn-warning btn-lg">Fetch Price</button>
+        <h1 className="title">
+          {cPair} {price}
+        </h1>
+        <div>
+          <input type="text" onChange={cryptoPair} />
+        </div>
+
+        <button onClick={fetchPrice} className="btn btn-warning btn-lg">
+          Search Price
+        </button>
       </div>
-
+      <div>
+        {" "}
+        <button onClick={getPrice} className="btn btn-warning btn-lg">
+          SHIB Price
+        </button>
+      </div>
     </div>
-  )
-
+  );
 }
 
-
 export default App;
-
-
