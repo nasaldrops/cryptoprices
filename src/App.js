3e5c31a65eb49ebf0c79bbeda66f0208b1969135
nasaldrops <hellokevinoconnor@gmail.com>
2022-10-31 19:37:07 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+
+  const [price, setPrice] = useState('');
+
+
+
+  const fetchPrice = async () => {
+    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=SHIBBUSD');
+    console.log(result.data.price)
+    setPrice(result.data.price)
+
+
+
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1 className="title"> { price } </h1>
+        <button onClick={fetchPrice} className="btn btn-warning btn-lg">Fetch Price</button>
+      </div>
+
     </div>
-  );
+  )
+
 }
 
+
 export default App;
+
+
