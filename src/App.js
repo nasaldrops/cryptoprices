0c77d64e08fac7a1a2975b0b298e89c1574da878
nasaldrops <hellokevinoconnor@gmail.com>
2022-11-02 09:40:13 +0000

    add LUNC button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
     setPrice(result.data.price)
 
 
+  };
+
+  const fetchPriceLUNC = async () => {
+    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=LUNCBUSD');
+    console.log(result.data.price)
+    setPrice(result.data.price)
 
 
   };
@@ -24,7 +30,9 @@ function App() {
     <div className="container">
       <div>
         <h1 className="title"> { price } </h1>
-        <button onClick={fetchPrice} className="btn btn-warning btn-lg">Fetch Price</button>
+        <button onClick={fetchPrice} className="btn btn-warning btn-lg">SHIB Price</button>
+        <br />
+        <button onClick={fetchPriceLUNC} className="btn btn-warning btn-lg">LUNC Price</button>
       </div>
 
     </div>
