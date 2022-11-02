dc6102548e87c045a60e15636f44639aa21b821c
nasaldrops <hellokevinoconnor@gmail.com>
2022-11-02 13:07:47 +0000

    add ETH and BTC buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
 
   const [shibPrice, setShibPrice] = useState('');
   const [luncPrice, setLuncPrice] = useState('');
+  const [ethPrice, setEthPrice] = useState('');
+  const [btcPrice, setBtcPrice] = useState('');
 
 
 
@@ -27,14 +29,36 @@ function App() {
 
   };
 
+  const fetchPriceETH = async () => {
+    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=ETHBUSD');
+    console.log(result.data.price)
+    setEthPrice(result.data.price)
+
+
+  };
+
+  const fetchPriceBTC = async () => {
+    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCBUSD');
+    console.log(result.data.price)
+    setBtcPrice(result.data.price)
+
+
+  };
+
   return (
     <div className="container">
       <div>
-        <h1 className="title"> { shibPrice } </h1>
+        <h1 className="title"> {shibPrice} </h1>
         <button onClick={fetchPrice} className="btn btn-warning btn-lg">SHIB Price</button>
-        <br/>
-        <h1 className="title"> { luncPrice } </h1>
+        <br />
+        <h1 className="title"> {luncPrice} </h1>
         <button onClick={fetchPriceLUNC} className="btn btn-warning btn-lg">LUNC Price</button>
+        <br />
+        <h1 className="title"> {ethPrice} </h1>
+        <button onClick={fetchPriceETH} className="btn btn-warning btn-lg">ETH Price</button>
+        <br />
+        <h1 className="title"> {btcPrice} </h1>
+        <button onClick={fetchPriceBTC} className="btn btn-warning btn-lg">BTC Price</button>
       </div>
 
     </div>
@@ -44,5 +68,3 @@ function App() {
 
 
 export default App;
-
-
