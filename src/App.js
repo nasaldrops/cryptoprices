557e6c8f80c7c9f1914102b362bbe84a0f1eece7
nasaldrops <hellokevinoconnor@gmail.com>
2023-10-25 17:18:29 +0100

    new crypto

--- src/App.js
+++ src/App.js
@@ -1,106 +1,99 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios';
-
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-
-  const [shibPrice, setShibPrice] = useState('');
-  const [luncPrice, setLuncPrice] = useState('');
-  const [ethPrice, setEthPrice] = useState('');
-  const [btcPrice, setBtcPrice] = useState('');
-  const [dogePrice, setDogePrice] = useState('');
-  const [pepePrice, setPepePrice] = useState('');
-  const [eurPrice, setEurPrice] = useState('');
-  
+  const [shibPrice, setShibPrice] = useState("");
+  const [luncPrice, setLuncPrice] = useState("");
+  const [ethPrice, setEthPrice] = useState("");
+  const [btcPrice, setBtcPrice] = useState("");
+  const [dogePrice, setDogePrice] = useState("");
+  const [pepePrice, setPepePrice] = useState("");
+  const [eurPrice, setEurPrice] = useState("");
 
   const fetchPrice = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=SHIBBUSD');
-    console.log(result.data.price)
-    setShibPrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=SHIBBUSD"
+    );
+    console.log(result.data.price);
+    setShibPrice(result.data.price);
   };
 
   const fetchPriceLUNC = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=LUNCBUSD');
-    console.log(result.data.price)
-    setLuncPrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=LUNCBUSD"
+    );
+    console.log(result.data.price);
+    setLuncPrice(result.data.price);
   };
 
   const fetchPriceETH = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=ETHBUSD');
-    console.log(result.data.price)
-    setEthPrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=ETHBUSD"
+    );
+    console.log(result.data.price);
+    setEthPrice(result.data.price);
   };
 
   const fetchPriceBTC = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCBUSD');
-    console.log(result.data.price)
-    setBtcPrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=BTCBUSD"
+    );
+    console.log(result.data.price);
+    setBtcPrice(result.data.price);
   };
 
   const fetchPriceDOGE = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=DOGEBUSD');
-    console.log(result.data.price)
-    setDogePrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=DOGEBUSD"
+    );
+    console.log(result.data.price);
+    setDogePrice(result.data.price);
   };
 
   const fetchPricePEPE = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=PEPEUSDT');
-    console.log(result.data.price)
-    setPepePrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=PEPEUSDT"
+    );
+    console.log(result.data.price);
+    setPepePrice(result.data.price);
   };
 
   const fetchPriceEURO = async () => {
-    const result = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=EURBUSD');
-    console.log(result.data.price)
-    setEurPrice(result.data.price)
-
-
+    const result = await axios.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=EURBUSD"
+    );
+    console.log(result.data.price);
+    setEurPrice(result.data.price);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetchPrice();
+      fetchPriceLUNC();
+      fetchPriceETH();
+      fetchPriceBTC();
+      fetchPriceDOGE();
+      fetchPricePEPE();
+      fetchPriceEURO();
+    }, 1000); // Fetch every 1000 ms (1 second)
 
+    return () => clearInterval(intervalId); // Clear the interval when the component is unmounted
+  }, []);
 
   return (
     <div className="container">
-      <div>
-        <h1 className="title"> {shibPrice} </h1>
-        <button onClick={fetchPrice} className="btn btn-warning btn-lg">SHIB Price</button>
-        <br />
-        <h1 className="title"> {pepePrice} </h1>
-        <button onClick={fetchPricePEPE} className="btn btn-warning btn-lg">PEPE Price</button>
-        <br />
-        <h1 className="title"> {luncPrice} </h1>
-        <button onClick={fetchPriceLUNC} className="btn btn-warning btn-lg">LUNC Price</button>
-        <br />
-        <h1 className="title"> {dogePrice} </h1>
-        <button onClick={fetchPriceDOGE} className="btn btn-warning btn-lg">DOGE Price</button>
-        <br />
-        <h1 className="title"> {ethPrice} </h1>
-        <button onClick={fetchPriceETH} className="btn btn-warning btn-lg">ETH Price</button>
-        <br />
-        <h1 className="title"> {btcPrice} </h1>
-        <button onClick={fetchPriceBTC} className="btn btn-warning btn-lg">BTC Price</button>
-        <br />
-        <h1 className="title"> {eurPrice} </h1>
-        <button onClick={fetchPriceEURO} className="btn btn-warning btn-lg">EUR Price</button>
+      <div className="card">
+        <h1 className="title">SHIB: {shibPrice}</h1>
+        <h1 className="title">PEPE: {pepePrice}</h1>
+        <h1 className="title">LUNC: {luncPrice}</h1>
+        <h1 className="title">DOGE: {dogePrice}</h1>
+        <h1 className="title">ETH: {ethPrice}</h1>
+        <h1 className="title">BTC: {btcPrice}</h1>
+        <h1 className="title">EUR: {eurPrice}</h1>
       </div>
-
     </div>
-  )
-
+  );
 }
 
 export default App;
